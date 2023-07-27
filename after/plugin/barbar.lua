36332f9210602be60e7c3d473d00079ee1f49d4a
Vinicius Barbosa <vininew921@hotmail.com>
2023-07-27 08:28:14 -0300

    Changed tab swap shortcuts

--- after/plugin/barbar.lua
+++ after/plugin/barbar.lua
@@ -1,5 +1,5 @@
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set('n', '<A-n>', ':BufferNext<CR>', opts)
-vim.keymap.set('n', '<A-p>', ':BufferPrevious<CR>', opts)
+vim.keymap.set('n', '<A-.>', ':BufferNext<CR>', opts)
+vim.keymap.set('n', '<A-,>', ':BufferPrevious<CR>', opts)
 vim.keymap.set('n', '<A-w>', ':BufferClose!<CR>', opts)
