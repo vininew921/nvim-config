6b5958006a5d9331637e5793f109ae1c5de79bc0
Vinicius Barbosa <Vinicius.Barbosa@fit-tecnologia.org.br>
2023-07-27 16:57:10 -0300

    added go to implementation and fixed requirements

--- requirements.ps1
+++ requirements.ps1
@@ -1,6 +1,3 @@
-irm get.scoop.sh | iex
 
-git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
-
-scoop install ripgrep -Y
+scoop install ripgrep
 scoop install gcc
