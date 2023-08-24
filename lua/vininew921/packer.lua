82bdde4ccef11e20b95a0efe9030fcedfc4e023a
Vinicius Barbosa <vininew921@hotmail.com>
2023-08-23 23:45:22 -0300

    changed theme

--- lua/vininew921/packer.lua
+++ lua/vininew921/packer.lua
@@ -12,14 +12,6 @@ return require('packer').startup(function(use)
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
-    use({
-        'catppuccin/nvim',
-        as = 'catppuccin',
-        config = function()
-            vim.cmd('colorscheme catppuccin')
-        end
-    })
-
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground', { run = ':TSUpdate' })
     use('mbbill/undotree')
