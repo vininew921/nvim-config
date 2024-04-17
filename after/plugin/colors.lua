10b321ddba134f43645f57df6d221c80d07db533
Vinicius Barbosa <vininew921@hotmail.com>
2024-04-17 20:48:05 -0300

    Removed barbar and other HL groups bg colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,6 +4,15 @@ function ColorMyPencils(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+
+    local buffer_groups = vim.fn.getcompletion("Buffer*", "highlight")
+    for _, group in ipairs(buffer_groups) do
+        vim.api.nvim_set_hl(0, group, { bg = "none" })
+    end
 end
 
 ColorMyPencils()
